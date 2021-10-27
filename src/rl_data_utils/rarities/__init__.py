5bf3f62aeca940e3504581c52f0e3da5cf4ace34
TheVicio <thevicio27@gmail.com>
2021-10-27 18:47:18 -0300

    fix import again lol

--- src/rl_data_utils/rarities/__init__.py
+++ src/rl_data_utils/rarities/__init__.py
@@ -1,5 +1,5 @@
 from rl_data_utils.rarities.rarities import Rarity, all_are_rarities, compare_rarities, contains_rarities,\
-    contains_rarities_in_list, get_rarity_in_string, get_respective_rarity, get_rgba_rarity, is_rarity, is_rarity_list,\
+    contains_rarities_in_list, get_rarity_in_string, get_respective_rarity, get_rgb_rarity, is_rarity, is_rarity_list,\
     validate_rarities_list, validate_rarity
 from rl_data_utils.rarities.is_functions import *
 from rl_data_utils.rarities.contains import *
