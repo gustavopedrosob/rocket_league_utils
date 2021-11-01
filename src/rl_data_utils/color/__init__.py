7f79d3ece2ae5c75789399ba15ca2e9fa12d977c
TheVicio <thevicio27@gmail.com>
2021-10-31 21:34:24 -0300

    Fix import error

--- src/rl_data_utils/color/__init__.py
+++ src/rl_data_utils/color/__init__.py
@@ -1,5 +1,5 @@
 from rl_data_utils.color.color import Color, all_are_colors, compare_colors, contains_colors,\
-    contains_colors_in_list, get_color_in_string, get_hex_colors, get_respective_color, is_color, is_certify_list,\
+    contains_color_in_list, get_color_in_string, get_hex_colors, get_respective_color, is_color, is_certify_list,\
     validate_color, validate_colors_list
 from rl_data_utils.color.is_functions import *
 from rl_data_utils.color.contains import *
