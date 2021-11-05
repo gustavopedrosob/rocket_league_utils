6404506bbfbb506878702862fede69cc94c626b7
TheVicio <thevicio27@gmail.com>
2021-11-05 19:27:46 -0300

    Concluindo o commit anterior.

--- src/rl_data_utils/inventory/inventory.py
+++ src/rl_data_utils/inventory/inventory.py
@@ -1,20 +1,18 @@
 from rl_data_utils.names.names import ABCNames
-from rl_data_utils.types.types import ABCStrTypes
-from rl_data_utils.colors.colors import ABCStrColors
-from rl_data_utils.certificates.certificates import ABCStrCertificates
+from rl_data_utils.types.types import ABCTypes
+from rl_data_utils.colors.colors import ABCColors
+from rl_data_utils.certificates.certificates import ABCCertificates
 from rl_data_utils.rarities.rarities import ABCRarities
 from rl_data_utils.quantities.quantities import ABCQuantities
-from rl_data_utils.item.item import ABCItem
-from rl_data_utils.items.items_search import ABCItemsSearch
 
 
-class Inventory(ABCNames, ABCStrTypes, ABCStrColors, ABCStrCertificates, ABCRarities, ABCQuantities, ABCItemsSearch):
-    def __init__(self, items: list[ABCItem]):
+class Inventory(ABCNames, ABCTypes, ABCColors, ABCCertificates, ABCRarities, ABCQuantities):
+    def __init__(self, items):
         self.items = items
 
     def get_items(self):
         return self.items
 
-    def set_items(self, items: list[ABCItem]):
+    def set_items(self, items):
         self.items = items
 
